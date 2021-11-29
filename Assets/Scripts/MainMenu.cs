30d75c325004887356eef96c7ffe0d68af259db1
Sepher <misunra8@gmail.com>
2021-11-29 09:11:58 +0300

    v1.0 WWise Edition

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject infoTab;
+    public GameObject menuTab;
+    public GameObject mainCanvas;
+    public GameObject endLevelCanvas;
+
+    private void Awake()
+    {
+        menuTab.SetActive(false);
+        mainCanvas.SetActive(true);
+        endLevelCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (GameManager.levelClear || GameManager.levelLose)
+        {
+            menuTab.SetActive(false);
+            mainCanvas.SetActive(false);
+            endLevelCanvas.SetActive(true);
+        }
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void Info()
+    {
+        infoTab.active = !infoTab.active;
+
+    }
+    public void Exit()
+    {
+        Application.Quit(0);
+    }
+
+    public void OpenMenu()
+    {
+        menuTab.active = !menuTab.active;
+        mainCanvas.active = !mainCanvas.active;
+        Time.timeScale = menuTab.active ? 0f : 1f;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
